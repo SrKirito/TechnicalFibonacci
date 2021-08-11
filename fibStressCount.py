70ba117533b0a81d1e0a6efab25719aeb76f3a06
Nagaze <enzofrancenzo1@gmail.com>
2021-08-11 18:52:20 -0300

    Now the Whole program works in a single function, that is quite more slow in fast calculations, but more faster in long calculations

--- fibStressCount.py
+++ fibStressCount.py
@@ -2,38 +2,44 @@ from datetime import datetime
 from datetime import timedelta
 from datetime import time
 
-start_time = datetime.now()
+def fibFunc():
+   
+   nterms = int(input("What's the initial term? "))
 
-nterms = int(input("What's the initial term? "))
+   nterms += 1
 
-nterms += 1
+   n1, n2 = 0, 1
+   count = 0
 
-n1, n2 = 0, 1
-count = 0
+   start_time = datetime.now()
 
-if nterms <= 0:
-   print("Give a valid positive number")
-elif nterms == 1:
-   print("Fibonacci Sequence > ",nterms,":")
-   print(n1)
-else:
-   print("Fibonacci Sequence:")
-   while count < nterms:
-       print("{} ".format(n1))
-       print("")
-       nth = n1 + n2
-       n1 = n2
-       n2 = nth
-       count += 1
+   if nterms <= 0:
+      print("Give a valid positive number")
+   elif nterms == 1:
+      print("Fibonacci Sequence > ",nterms,":")
+      print(n1)
+   else:
+      print("Fibonacci Sequence:")
+      while count < nterms:
+         print("{} ".format(n1))
+         print("")
+         nth = n1 + n2
+         n1 = n2
+         n2 = nth
+         count += 1
 
-def millis():
-   dt = datetime.now() - start_time
-   ms = int((dt.days * 24 * 60 * 60 + dt.seconds) * 1000 / 1000 + dt.microseconds)
-   s = (dt.days * 24 * 60 * 60 + dt.seconds)
-   m = dt.microseconds
-   return("{}.{}/{}".format(s,m,ms))
+   def millis():
+      dt = datetime.now() - start_time
+      ms = int((dt.days * 24 * 60 * 60 + dt.seconds) * 1000 / 1000 + dt.microseconds)
+      s = (dt.days * 24 * 60 * 60 + dt.seconds)
+      m = dt.microseconds
+      return("{}.{}/{}".format(s,m,ms))
 
-nterms -= 1
+   nterms -= 1
 
-print("\nInitial term of Fibonacci Sequence:\t\t\t\t\t", nterms)
-print("Calculation duration: \t\t\t\t\t\t\t {}\t\t\t\t\t       (Seconds/Milliseconds)".format(millis()))
+   print("\nInitial term of Fibonacci Sequence:\t\t\t\t\t", nterms)
+   print("Calculation duration: \t\t\t\t\t\t\t {}\t\t\t\t\t       (Seconds/Milliseconds)".format(millis()))
+
+   return 0
+
+fibFunc()
